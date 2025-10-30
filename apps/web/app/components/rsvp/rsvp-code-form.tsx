288dd57a361116f6d4d39cc5b1dcd1985be913a1
Hunter Jennings <jenningsdhunter@gmail.com>
2025-10-29 21:42:01 -0400

    WIP: more form stuff

--- apps/web/app/components/rsvp/rsvp-code-form.tsx
+++ apps/web/app/components/rsvp/rsvp-code-form.tsx
@@ -2,20 +2,24 @@
 
 import { useActionState } from "react";
 import { verifyCode } from "~/app/lib/auth/auth.actions";
+import { copy } from "~/app/styles/text.styles";
+import { Button } from "../button";
 
-export function RsvpCodeForm() {
-  const verifyCodeWithCode = verifyCode.bind(
-    null,
-    // TODO: code goes here
-    "",
-  );
-  const [state, submitAction, isPending] = useActionState(verifyCodeWithCode, { status: "idle" });
+export function RsvpVerifyCodeForm() {
+  const [state, submitAction, isPending] = useActionState(verifyCode, { status: "idle" });
 
   return (
-    <form action={submitAction}>
-      <button type="submit" className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-        Verify Code
-      </button>
+    <form action={submitAction} className="flex flex-col items-center gap-y-6 text-center">
+      <div className="flex flex-col items-center gap-y-4 text-center">
+        <h2 className={copy()}>Enter your super secret code</h2>
+        <input
+          type="text"
+          name="code"
+          placeholder="Enter the secret code"
+          className="h-10 w-full min-w-0 rounded-lg border border-black/10 bg-transparent px-3 py-1 text-base/none text-gray-700 outline-none transition-all duration-200 hover:border-gray-400 focus:border-gray-400 focus:shadow-[0px_0px_1px_2px_#D2D2D3]"
+        />
+      </div>
+      <Button disabled={isPending}>Verify Code</Button>
     </form>
   );
 }
---
+++ apps/web/app/components/rsvp/rsvp-form.tsx
@@ -0,0 +1,7 @@
+export function RsvpForm() {
+  return (
+    <div>
+      <h1>RSVP Form</h1>
+    </div>
+  );
+}
