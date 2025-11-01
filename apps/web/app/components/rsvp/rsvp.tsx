1c9723a0476bec612b5e0acd13d253e65894f49e
Hunter Jennings <jenningsdhunter@gmail.com>
2025-11-01 10:48:43 -0400

    wip: added actual rsvp route

--- apps/web/app/components/rsvp/rsvp.tsx
+++ apps/web/app/components/rsvp/rsvp.tsx
@@ -1,6 +1,6 @@
+import Link from "next/link";
 import { getAuthState } from "~/app/lib/auth/auth.helpers";
 import { RsvpVerifyCodeForm } from "./rsvp-code-form";
-import { RsvpForm } from "./rsvp-form";
 import { RsvpPartyLookup } from "./rsvp-party-lookup";
 
 export async function Rsvp() {
@@ -10,8 +10,13 @@ export async function Rsvp() {
     <div>
       {(() => {
         switch (true) {
+          // TODO: This should be a route that gets redirected to on the server, not in the modal
           case state.authorized && state.partyId != null: {
-            return <RsvpForm />;
+            return (
+              <p>
+                Should redirect, but <Link href="/rsvp"></Link> here's a link
+              </p>
+            );
           }
           case state.authorized && state.partyId == null: {
             return <RsvpPartyLookup />;
