f46dc7687a2dff5d1d70f24f456d11a1b209df39
Hunter Jennings <jenningsdhunter@gmail.com>
2025-11-01 18:58:52 -0400

    WIP

--- apps/web/app/components/rsvp/rsvp-party-lookup.tsx
+++ apps/web/app/components/rsvp/rsvp-party-lookup.tsx
@@ -11,7 +11,7 @@ export function RsvpPartyLookup() {
   return (
     <form action={submitAction} className="flex flex-col items-center gap-y-6 px-4 text-center">
       <div className="flex flex-col items-center gap-y-4 pt-14 text-center">
-        <h2 className={chunky()}>Party Lookup</h2>
+        <h2 className={chunky()}>Guest Lookup</h2>
         <p className={copy({ className: "text-pretty" })}>
           Please enter the first and last name of one member of your party below.
         </p>
@@ -19,14 +19,22 @@ export function RsvpPartyLookup() {
           <label htmlFor="full_name" className="sr-only">
             Full Name:
           </label>
-          <Input className="w-full" type="text" autoComplete="name" name="full_name" placeholder="Enter a full name" />
+          <Input
+            className="w-full"
+            type="text"
+            autoComplete="name"
+            name="full_name"
+            minLength={2}
+            required
+            placeholder="Enter a full name"
+          />
         </div>
         <p className={copy({ className: "text-pretty text-text-secondary text-xs" })}>
           Ex. Hunter Jennings (not The Jennings Family or Dr. & Mr. Jennings)
         </p>
       </div>
       <Button type="submit" disabled={isPending}>
-        Search Guest List
+        Search Guests
       </Button>
       <div className="h-10" />
     </form>
